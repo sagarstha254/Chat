14dd34a5bfe2d4d24c943c4a0e1f382e6f140f62
sagarstha254 <sagarstha254@gmail.com>
2025-01-01 23:26:40 +0545

    modify(route): routes with express validation

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,19 +1,62 @@
 import express from "express";
-import { isAuthenticated } from "../middlewares/auth.js";
 import {
   addMembers,
+  deleteChat,
+  getChatDetails,
+  getMessages,
   getMyChats,
   getMyGroups,
+  leaveGroup,
   newGroupChat,
+  removeMember,
+  renameGroup,
+  sendAttachments,
 } from "../controllers/chat.js";
+import {
+  addMemberValidator,
+  chatIdValidator,
+  newGroupValidator,
+  removeMemberValidator,
+  renameValidator,
+  sendAttachmentsValidator,
+  validateHandler,
+} from "../lib/validators.js";
+import { isAuthenticated } from "../middlewares/auth.js";
+import { attachmentsMulter } from "../middlewares/multer.js";
 
 const app = express.Router();
 
 app.use(isAuthenticated);
 
-app.post("/new", newGroupChat);
+app.post("/new", newGroupValidator(), validateHandler, newGroupChat);
 app.get("/my", getMyChats);
 app.get("/my/groups", getMyGroups);
-app.put("/addmembers", addMembers);
-//2:40:27 / 6:58:35s
+app.put("/addmembers", addMemberValidator(), validateHandler, addMembers);
+app.put(
+  "/removemember",
+  removeMemberValidator(),
+  validateHandler,
+  removeMember
+);
+app.delete("/leave/:id", chatIdValidator(), validateHandler, leaveGroup);
+
+//send attachments
+app.post(
+  "/message",
+  attachmentsMulter,
+  sendAttachmentsValidator(),
+  validateHandler,
+  sendAttachments
+);
+
+//get messages
+app.get("/message/:id", chatIdValidator(), validateHandler, getMessages);
+
+//get chat details, rename, delete
+app
+  .route("/:id")
+  .get(chatIdValidator(), validateHandler, getChatDetails)
+  .put(renameValidator(), validateHandler, renameGroup)
+  .delete(chatIdValidator(), validateHandler, deleteChat);
+
 export default app;
--- server/routes/user.js
+++ server/routes/user.js
@@ -8,14 +8,18 @@ import {
   searchUser,
 } from "../controllers/user.js";
 import { isAuthenticated } from "../middlewares/auth.js";
+import {
+  loginValidator,
+  registerValidator,
+  validateHandler,
+} from "../lib/validators.js";
 
 const app = express.Router();
-app.post("/new", singleAvatar, newUser);
-app.post("/login", login);
+app.post("/new", singleAvatar, registerValidator(), validateHandler, newUser);
+app.post("/login", loginValidator(), validateHandler, login);
 
 app.use(isAuthenticated);
 app.get("/me", getMyProfile);
 app.get("/logout", logout);
 app.get("/search", searchUser);
 export default app;
-

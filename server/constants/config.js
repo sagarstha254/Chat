3621fefeafd97fe0a974791d88ad1f6797a31e40
sagarstha254 <sagarstha254@gmail.com>
2025-01-12 14:00:30 +0545

    feat(admin): admin server handler

---
+++ server/constants/config.js
@@ -0,0 +1,12 @@
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:4173",
+    process.env.CLIENT_URL,
+  ],
+  credentials: true,
+};
+
+const CHATTU_TOKEN = "chattu-token";
+
+export { corsOptions, CHATTU_TOKEN };
